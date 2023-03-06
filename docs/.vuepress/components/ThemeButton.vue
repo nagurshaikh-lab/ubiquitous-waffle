32146ea2f1b366a053b545f41314b13ccc96324d
Nagur <nagur.shaikh@hotmail.com>
2023-03-06 16:43:21 +0530

    Initial commit

---
+++ docs/.vuepress/components/ThemeButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="btn" :style="{ color: '#fff', backgroundColor: (primary && theme.primaryColor) || (secondary && theme.secondaryColor)  }">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  inject: {
+    theme: {
+      default: {},
+    },
+  },
+  props: {
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    secondary: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
